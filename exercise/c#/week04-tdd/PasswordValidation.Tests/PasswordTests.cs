25b7e7399cb8dd31a80375e27f79c8b360caaf18
cedric <cedric.meleard@gmail.com>
2024-10-26 17:23:21 +0200

    Test passed for min length

---
+++ exercise/c#/week04-tdd/PasswordValidation.Tests/PasswordTests.cs
@@ -0,0 +1,42 @@
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace PasswordValidation.Tests;
+
+public class PasswordTests
+{
+    [Property]
+    public Property Password_With_MoreThan8Characters_ShouldBe_Valid(string? entry)
+    {
+        var property = () =>
+        {
+             var result = Password.TryCreate(entry, out Password password);
+             return result;
+        };
+        return property.When(entry is { Length: >= 8 });
+    }
+
+    [Theory]
+    [InlineData("aaa\\018\\006")]
+    [InlineData("\\026\\0162(t9[Q\\029")]
+    public void PasswordWithEscapes_ShouldNotBe_Valid(string? entry)
+    {
+        var result = Password.TryCreate(entry, out Password password);
+        
+        Assert.False(result);
+        Assert.Null(password);
+    }
+    
+    [Fact]
+    public void StrongPassword_ShouldBeValid()
+    {
+        var entry = "passowrd";
+        var result = Password.TryCreate(entry, out Password password);
+        
+        Assert.True(result);
+        Assert.NotNull(password);
+        Assert.Equal(entry, password.Value);
+    }
+
+}
